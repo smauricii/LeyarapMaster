2440dad4e6bf74b1bdfa6fb1f2ed3dd10b9b7369
smauricii <schamorro28@uan.edu.co>
2021-09-20 16:44:48 -0500

    prueba

--- app/src/main/java/com/edu/uan/android/leyarap/InicioActicity.kt
+++ app/src/main/java/com/edu/uan/android/leyarap/InicioActicity.kt
@@ -22,7 +22,7 @@ class InicioActicity : AppCompatActivity(), View.OnClickListener {
 
         //sesionActiva()
         //iniciolayout.visibility = View.VISIBLE
-        //ola
+        //ola buen día
         texto_inicio = findViewById(R.id.textoinicio)
         texto_inicio!!.typeface = Typeface.createFromAsset(assets, "fonts/lellaraptitle.otf")
 
