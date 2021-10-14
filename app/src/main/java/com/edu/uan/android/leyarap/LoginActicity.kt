04c3f75f921730fbb6205b0ac855c3021dba922d
smauricii <schamorro28@uan.edu.co>
2021-10-13 20:10:40 -0500

    cambio develop

--- app/src/main/java/com/edu/uan/android/leyarap/LoginActicity.kt
+++ app/src/main/java/com/edu/uan/android/leyarap/LoginActicity.kt
@@ -88,7 +88,6 @@ class LoginActicity : AppCompatActivity() {
                 }
             }
         }
-//cambios hola
         btn_ingresar.setOnClickListener {
             if (editCorreo.text.isNotEmpty() && editPassword.text.isNotEmpty()) {
                 FirebaseAuth.getInstance().signInWithEmailAndPassword(
